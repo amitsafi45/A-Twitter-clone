b3c6ee5ee755a9a95f348fa0a2e749ba51aa54e0
amitsafi45 <safiamit45@gmail.com>
2022-11-30 01:23:33 +0545

    feat: create connection with database through typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
+import 'dotenv/config'
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
 import { HashtagsController } from './hashtags/hashtags.controller';
 import { UsersController } from './users/users.controller';
-
+import {TypeOrmModule} from '@nestjs/typeorm'
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port:Number( process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [],
+    logging:true,
+    synchronize: true,})],
   controllers: [AppController, PostsController, HashtagsController, UsersController],
   providers: [AppService],
 })
