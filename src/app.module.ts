dcc660484856e109557f82e7dff98a20f51126f7
amitsafi45 <safiamit45@gmail.com>
2022-11-29 23:35:58 +0545

    feat: create users,post,hashtags controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
+import { HashtagsController } from './hashtags/hashtags.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PostsController, HashtagsController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
