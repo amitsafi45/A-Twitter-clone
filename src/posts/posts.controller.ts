316e05ed7f7a0c58a0655589493d3bc636421f94
amitsafi45 <safiamit45@gmail.com>
2022-11-30 00:05:38 +0545

    test: check posts controller work or not

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
-@Controller('posts')
-export class PostsController {}
+@Controller('api/posts')
+export class PostsController {
+
+//Get all post
+    @Get()
+    async gets():Promise<string>{
+      
+        return 'Get all post'
+    }
+
+    //Get details of a post
+    @Get(':postID')
+    async get():Promise<string>{
+        return 'Get details of a post'
+    }
+
+   //Create a new post
+
+   @Post()
+   async create():Promise<string>{
+    return 'Create a new post'
+   }
+
+   //Delete a given post id
+
+   @Delete(':postID')
+   async delete():Promise<string>{
+    return 'Delete a given post id'
+   }
+
+   //Like the given post
+
+   @Put(':postID/like')
+   async likePost():Promise<string>{
+        return 'Like the given post'
+   }
+
+   //Un-like the given post
+
+   @Delete(':postID/like')
+   async unlikePost():Promise<string>{
+    return 'Un-like the given post'
+   }
+
+
+
+}
