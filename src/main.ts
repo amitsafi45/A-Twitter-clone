9f43f420309e7f1668a882892159305e1b3a2ef3
amitsafi45 <safiamit45@gmail.com>
2022-11-30 22:07:51 +0545

    feat: setup config for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  const config = new DocumentBuilder()
+  .setTitle('A-Twitter-Clone')
+  .setDescription('A-Twitter-Clone Backend API Project')
+  .setVersion('1.0')
+  .addTag('Backend API')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
